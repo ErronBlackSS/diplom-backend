157b0bac1b674a6f39e63ec4fb6d1dc8ca573937
Mikhail Tovt <mikhail.tovt.02@mail.com>
2023-03-02 18:42:53 +0300

    Подключение приложения к postgresql + создание тестовых таблиц

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env' })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}

033a30477e6ecae26967014ef718ad80ac942c1d
Mikhail Tovt <mikhail.tovt.02@mail.com>
2023-03-02 18:25:45 +0300

    Установлена ORM для работы с базой данных + удалены лишние файлы

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3333;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+
+  const config = new DocumentBuilder()
+    .setTitle('Дипломная работа')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .addTag('Diplom')
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+
+  SwaggerModule.setup('./api/docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
